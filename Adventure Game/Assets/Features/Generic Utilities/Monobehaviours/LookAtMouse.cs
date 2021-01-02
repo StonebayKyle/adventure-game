5eeeb511257b280cbf9fbc3975455c738ace24c0
StonebayKyle <stonebay.kyle@gmail.com>
2021-01-01 17:24:28 -0700

    Adds an initial laser object, which is a child of the player GameObject, points towards the mouse position, and can be (not fully implemented) centered on different points

---
+++ Adventure Game/Assets/Features/Generic Utilities/Monobehaviours/LookAtMouse.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+// attach this to any object that needs to point towards the mouse by rotating
+public class LookAtMouse : MonoBehaviour
+{
+    [Tooltip("The current camera. Used for converting between pixel coordinates and world coordinates.")]
+    public Camera cam;
+    
+    // code inspired by Indie Nuggets and Brackeys on YouTube
+    private void Update()
+    {
+        // vector subtraction gets a vector pointing from the first vector to the second.
+        Vector3 lookDir = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position; // math done in world coordinates. Pixel coordinates are also possible, as long as it's consistent.
+
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg; // get the angle to point towards.
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward); // Vector3.forward as the axis because that represents the z axis (which is typically for rotating in 2D)
+    }
+
+
+}
---
+++ Adventure Game/Assets/Features/Player/Equipables/Laser/LaserController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+    [Header("Holding Points")]
+    [SerializeField]
+    private Transform leftHoldPoint;
+    [SerializeField]
+    private Transform rightHoldPoint;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        UpdatePosition();
+    }
+
+    private void UpdatePosition()
+    {
+        // TODO targetPosition depends on if the player is facing left or right.
+        Vector2 targetPosition = rightHoldPoint.position;
+        rb.MovePosition(targetPosition);
+    }
+}
+
