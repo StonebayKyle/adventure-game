d1667ad668e7d5dbdecb3d16bf8f5f7f82776bd3
StonebayKyle <stonebay.kyle@gmail.com>
2021-01-06 09:04:32 -0700

    Adds firing bullets from the laser

--- Adventure Game/Assets/Features/Player/Equipables/Laser/LaserController.cs
+++ Adventure Game/Assets/Features/Player/Equipables/Laser/LaserController.cs
@@ -6,6 +6,15 @@ public class LaserController : MonoBehaviour
 {
     [Tooltip("The transform point for where the laser should be bound to positionally. This is intended for where the laser would be held from (i.e. hands, pedestal, etc).")]
     public Transform holdPoint;
+    [Header("Bullet")]
+    [Tooltip("The transform point for where the laser should shoot from.")]
+    public Transform firePoint;
+    [Tooltip("The prefab of what the laser should shoot.")]
+    public GameObject bulletPrefab;
+    [Tooltip("How much force should be applied to the bullet when the laser is fired.")]
+    public float bulletFireForce = 20f;
+
+    private bool shootPressed = false;
 
     private Rigidbody2D rb;
 
@@ -23,12 +32,16 @@ public class LaserController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        shootPressed = Input.GetButtonDown("Fire1");
     }
 
     private void FixedUpdate()
     {
         UpdatePosition();
+        if (shootPressed)
+        {
+            Shoot();
+        }
     }
 
     private void UpdatePosition()
@@ -36,5 +49,14 @@ public class LaserController : MonoBehaviour
         Vector2 targetPosition = holdPoint.position;
         rb.MovePosition(targetPosition);
     }
+
+    private void Shoot()
+    {
+        // code inspired by Brackeys
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
+        bulletRigidbody.AddForce(firePoint.right * bulletFireForce, ForceMode2D.Impulse);
+    }
+
 }
 
