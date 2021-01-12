fd8ee3f17c7cf94e6bfcf6ba38d3af4f641e2ca6
Arjun Iyer <iyera2@s.dcsdk12.org>
2021-01-11 19:10:42 -0700

    Added destructible tiles code.

---
+++ Adventure Game/Assets/Features/World/Tilemap/Palettes/DestructableTiles.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DestructableTiles : MonoBehaviour{
+  public Tilemap destructableTilemap;
+
+  public void Start(){
+    destructableTilemap = GetComponent<Tilemap>();
+  }
+
+  public void OnCollisionEnter2D(Collision2D collision){
+    if(collision.gameObject.CompareTag("Player")){
+      Vector3 hitPosition = Vector2.zero;
+      foreach(ContactPoint2D hit in collision.contacts){
+        hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
+        hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
+        destructableTilemap.SetTile(destructableTilemap.WorldToCell(hitPosition), null);
+      }
+    }
+  }
+}
