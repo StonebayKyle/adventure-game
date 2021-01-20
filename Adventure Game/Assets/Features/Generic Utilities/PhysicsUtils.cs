989dc4a72021bbc2ee9fbee032860d227bf58f7d
StonebayKyle <stonebay.kyle@gmail.com>
2021-01-20 09:31:55 -0700

    Adds a clamp to the applying force's current velocity, limiting the amount of velocity change allowed when the player is moving quicker than the target (max) velocity (except for when the target is 0, explained in comments)

--- Adventure Game/Assets/Features/Generic Utilities/PhysicsUtils.cs
+++ Adventure Game/Assets/Features/Generic Utilities/PhysicsUtils.cs
@@ -41,8 +41,12 @@ public class PhysicsUtils
     public static void ApplyForceTowards(Rigidbody2D rigidbody, float targetVelocity, float accelerationTime)
     {
         float xVelocity = 0.0f;
-        
-        float currentVelocity = rigidbody.velocity.x;
+
+        //float currentVelocity = rigidbody.velocity.x;
+
+        // clamps the current velocity used for force applying between -target and +target, so only a certain amount of force difference is allowed to happen
+            // except for when the target is 0. In that case, keep it as velocity directly so a change is allowed to happen (subtracting smoothedVelocity by 0 doesn't change velocity).
+        float currentVelocity = (targetVelocity == 0) ? rigidbody.velocity.x : Mathf.Clamp(rigidbody.velocity.x, -Mathf.Abs(targetVelocity), Mathf.Abs(targetVelocity));
 
         // calculate the target velocity for a single run through this method, which is smoothed.
         float smoothedVelocity = Mathf.SmoothDamp(currentVelocity, targetVelocity, ref xVelocity, accelerationTime, Mathf.Infinity, Time.fixedDeltaTime);
