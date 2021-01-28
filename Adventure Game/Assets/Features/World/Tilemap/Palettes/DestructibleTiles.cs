d7d4efb35e699b6f2b1dbc1307d9ce85d98a374d
StonebayKyle <stonebay.kyle@gmail.com>
2021-01-28 09:10:59 -0700

    Corrects "Destructable" spelling to "Destructible"

--- Adventure Game/Assets/Features/World/Tilemap/Palettes/DestructibleTiles.cs
+++ Adventure Game/Assets/Features/World/Tilemap/Palettes/DestructibleTiles.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class DestructableTiles : MonoBehaviour{
-  public Tilemap destructableTilemap;
+public class DestructibleTiles : MonoBehaviour{
+  public Tilemap destructibleTilemap;
 
   public void Start(){
-    destructableTilemap = GetComponent<Tilemap>();
+    destructibleTilemap = GetComponent<Tilemap>();
   }
 
   public void OnCollisionEnter2D(Collision2D collision){
@@ -16,7 +16,7 @@ public class DestructableTiles : MonoBehaviour{
       foreach(ContactPoint2D hit in collision.contacts){
         hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
         hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
-        destructableTilemap.SetTile(destructableTilemap.WorldToCell(hitPosition), null);
+        destructibleTilemap.SetTile(destructibleTilemap.WorldToCell(hitPosition), null);
       }
     }
   }
