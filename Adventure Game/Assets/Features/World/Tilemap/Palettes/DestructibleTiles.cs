df26507566211aefcb06b57ff2ea8f815cb128bc
StonebayKyle <stonebay.kyle@gmail.com>
2021-01-28 09:25:05 -0700

    Corrects spacing and replaces spaces with tabs in DestructibleTiles

--- Adventure Game/Assets/Features/World/Tilemap/Palettes/DestructibleTiles.cs
+++ Adventure Game/Assets/Features/World/Tilemap/Palettes/DestructibleTiles.cs
@@ -3,21 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class DestructibleTiles : MonoBehaviour{
-  public Tilemap destructibleTilemap;
+public class DestructibleTiles : MonoBehaviour
+{
+    public Tilemap destructibleTilemap;
 
-  public void Start(){
-    destructibleTilemap = GetComponent<Tilemap>();
-  }
-
-  public void OnCollisionEnter2D(Collision2D collision){
-    if(collision.gameObject.CompareTag("Player")){
-      Vector3 hitPosition = Vector2.zero;
-      foreach(ContactPoint2D hit in collision.contacts){
-        hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
-        hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
-        destructibleTilemap.SetTile(destructibleTilemap.WorldToCell(hitPosition), null);
-      }
+    public void Start()
+    {
+        destructibleTilemap = GetComponent<Tilemap>();
     }
-  }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Vector3 hitPosition = Vector2.zero;
+            foreach(ContactPoint2D hit in collision.contacts)
+            {
+                hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
+                hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
+                destructibleTilemap.SetTile(destructibleTilemap.WorldToCell(hitPosition), null);
+            }
+        }
+    }
 }
