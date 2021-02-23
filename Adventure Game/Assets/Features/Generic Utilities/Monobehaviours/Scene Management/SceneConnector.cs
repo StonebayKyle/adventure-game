f9ab93e560cfcb800e4de9cf8f6becc1824c8616
StonebayKyle <stonebay.kyle@gmail.com>
2021-02-23 16:34:18 -0700

    Adds a way to move between scenes in-game using objects in Scene Management folder. Use this for creating entry (SpawnPoint) and exit (ExitConnector) points that a gameObject with the SceneTransferable component can interact with and go between.

---
+++ Adventure Game/Assets/Features/Generic Utilities/Monobehaviours/Scene Management/SceneConnector.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class SceneConnector : MonoBehaviour
+{
+    [Tooltip("Name of which scene to connect to.")]
+    public string sceneName;
+    [Tooltip("Which SpawnPoint to send to in levelID scene.")]
+    public int spawnPointID;
+}
---
+++ Adventure Game/Assets/Features/Generic Utilities/Monobehaviours/Scene Management/SceneTransferable.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(DontDestroy))]
+[RequireComponent(typeof(Collider2D))]
+public class SceneTransferable : MonoBehaviour
+{
+    private int NextLevelSpawnPointID;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {   
+        SceneConnector connector = collision.gameObject.GetComponent<SceneConnector>();
+        if (connector != null)
+        {
+            Debug.LogWarning("Collision with connector!");
+            NextLevelSpawnPointID = connector.spawnPointID;
+            SceneManager.LoadScene(connector.sceneName); // Should make a loading screen or smoother transition.
+
+            SpawnPoint[] spawnPoints = Object.FindObjectsOfType<SpawnPoint>();
+            foreach (SpawnPoint spawnPoint in spawnPoints)
+            {
+                if (spawnPoint.spawnPointID == NextLevelSpawnPointID)
+                {
+                    transform.position = spawnPoint.transform.position;
+                    break;
+                }
+            }
+        }
+    }
+}
