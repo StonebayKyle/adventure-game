38ac45e842871941deeabf87bc30daa80215aaad
StonebayKyle <stonebay.kyle@gmail.com>
2021-02-23 18:16:03 -0700

    Improves tooltips on SceneManagement tools

--- Adventure Game/Assets/Features/Generic Utilities/Monobehaviours/Scene Management/SceneConnector.cs
+++ Adventure Game/Assets/Features/Generic Utilities/Monobehaviours/Scene Management/SceneConnector.cs
@@ -3,8 +3,8 @@
 [RequireComponent(typeof(Collider2D))]
 public class SceneConnector : MonoBehaviour
 {
-    [Tooltip("Name of which scene to connect to.")]
+    [Tooltip("Name of which scene to connect to (send the player to this scene).")]
     public string sceneName;
-    [Tooltip("Which SpawnPoint to send to in levelID scene.")]
+    [Tooltip("Which SpawnPoint to spawn the player at in the new scene.")]
     public int spawnPointID;
 }
--- Adventure Game/Assets/Features/Generic Utilities/Monobehaviours/Scene Management/SpawnPoint.cs
+++ Adventure Game/Assets/Features/Generic Utilities/Monobehaviours/Scene Management/SpawnPoint.cs
@@ -2,5 +2,6 @@
 
 public class SpawnPoint : MonoBehaviour
 {
+    [Tooltip("SpawnPointID of this SpawnPoint. Match this value with a value in a SceneConnector to connect to this SpawnPoint.")]
     public int spawnPointID;
 }
