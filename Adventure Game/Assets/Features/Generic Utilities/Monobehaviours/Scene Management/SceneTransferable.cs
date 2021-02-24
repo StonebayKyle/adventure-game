d1d960eb3cf7f23525679c754853ec97e45b442c
StonebayKyle <stonebay.kyle@gmail.com>
2021-02-23 18:03:42 -0700

    Fixes not spawning at a spawn point in the new scene by adding a delegate callback for when the new scene is finished loading. Only after the new scene is finished loading will the player teleport to the spawnpoint in the new scene.

--- Adventure Game/Assets/Features/Generic Utilities/Monobehaviours/Scene Management/SceneTransferable.cs
+++ Adventure Game/Assets/Features/Generic Utilities/Monobehaviours/Scene Management/SceneTransferable.cs
@@ -8,23 +8,52 @@ public class SceneTransferable : MonoBehaviour
     private int NextLevelSpawnPointID;
 
     public void OnTriggerEnter2D(Collider2D collision)
-    {   
+    {
+        FindAndStartLoad(collision);
+    }
+
+    private void FindAndStartLoad(Collider2D collision)
+    {
         SceneConnector connector = collision.gameObject.GetComponent<SceneConnector>();
         if (connector != null)
         {
             Debug.LogWarning("Collision with connector!");
             NextLevelSpawnPointID = connector.spawnPointID;
+            // When the scene is loaded, OnSceneLoaded will finish the process
             SceneManager.LoadScene(connector.sceneName); // Should make a loading screen or smoother transition.
+        }
+    }
 
-            SpawnPoint[] spawnPoints = Object.FindObjectsOfType<SpawnPoint>();
-            foreach (SpawnPoint spawnPoint in spawnPoints)
+    private void TransportToSpawnPoint()
+    {
+        SpawnPoint[] spawnPoints = FindObjectsOfType<SpawnPoint>();
+        Debug.LogWarning("Amount of spawnPoints: " + spawnPoints.Length);
+        foreach (SpawnPoint spawnPoint in spawnPoints)
+        {
+            Debug.LogWarning("Searching spawnPoints...");
+            if (spawnPoint.spawnPointID == NextLevelSpawnPointID)
             {
-                if (spawnPoint.spawnPointID == NextLevelSpawnPointID)
-                {
-                    transform.position = spawnPoint.transform.position;
-                    break;
-                }
+                Debug.LogWarning("Found equal spawnPoint!");
+                transform.position = spawnPoint.transform.position;
+                break;
             }
         }
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    // delegate that is called when the scene is loaded.
+    private void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
+    {
+        Debug.LogWarning("Load finished");
+        TransportToSpawnPoint();
     }
 }
