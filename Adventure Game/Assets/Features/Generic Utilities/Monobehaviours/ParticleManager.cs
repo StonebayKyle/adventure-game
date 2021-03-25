ceb1c8145f87c10001b883fb4b0bcef1d62ed7d1
StonebayKyle <stonebay.kyle@gmail.com>
2021-03-25 10:01:45 -0600

    Creates initial ParticleManager system, which is a monobehavior to support objects creating multiple ParticleSystems.

---
+++ Adventure Game/Assets/Features/Generic Utilities/Monobehaviours/ParticleManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+    public ParticleSystem updateParticleSystem;
+    public ParticleSystem explodeParticleSystem;
+
+    [System.NonSerialized]
+    public float explodeLifetime; // the explode particle's duration. Use to time gameobject destruction
+
+    public void Awake()
+    {
+        if (explodeParticleSystem != null)
+        {
+            explodeLifetime = explodeParticleSystem.main.duration;
+        }
+    }
+
+    private void StartParticleSystem(ParticleSystem particleSystem)
+    {
+        if (particleSystem == null || particleSystem.isPlaying) return;
+
+        particleSystem.Clear();
+        particleSystem.Play();
+    }
+
+    private void InstantiateParticleSystem(ParticleSystem particleSystem)
+    {
+        if (particleSystem == null) return;
+
+        Instantiate(particleSystem, transform.position, transform.rotation);
+    }
+
+    public void UpdateParticle()
+    {
+        StartParticleSystem(updateParticleSystem);
+    }
+
+    public void Explode()
+    {
+        InstantiateParticleSystem(explodeParticleSystem);
+    }
+}
