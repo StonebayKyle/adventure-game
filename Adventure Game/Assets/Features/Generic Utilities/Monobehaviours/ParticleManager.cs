337f524da5a856ad42b3034caa9be0927ada260a
StonebayKyle <stonebay.kyle@gmail.com>
2021-04-01 09:56:29 -0600

    Adds support for multiple followParticles for ParticleManager

--- Adventure Game/Assets/Features/Generic Utilities/Monobehaviours/ParticleManager.cs
+++ Adventure Game/Assets/Features/Generic Utilities/Monobehaviours/ParticleManager.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class ParticleManager : MonoBehaviour
 {
-    [Tooltip("A trail ParticleSystem. ParticleSystem is instantiated as a child of the gameobject's transform and emits from the object. A trail effect would be simulated in world space, and a following effect would be simulated in local space.")]
-    public ParticleSystem followParticleSystem;
+    [Tooltip("FollowParticleSystems follow the gameobject. A ParticleSystem is instantiated as a child of the gameobject's transform and emits from the object. A trail effect would be simulated in world space, and a following effect would be simulated in local space.")]
+    public ParticleSystem[] followParticleSystems;
 
-    private GameObject trailParticleGameObject;
-    private ParticleSystem objTrailParticleSystem; // the instantiated trail's particle system.
+    private GameObject[] trailParticleGameObjects;
+    private ParticleSystem[] objTrailParticleSystems; // the instantiated trail's particle systems.
 
     [Tooltip("ParticleSystem that instantiates when the object explodes.")]
     public ParticleSystem explodeParticleSystem;
@@ -44,33 +44,42 @@ public class ParticleManager : MonoBehaviour
 
     }
 
-    public void CreateFollowParticle()
+    public void CreateFollowParticles()
     {
-        trailParticleGameObject = Instantiate(followParticleSystem, transform.position, transform.rotation, transform).gameObject;
-        objTrailParticleSystem = trailParticleGameObject.GetComponent<ParticleSystem>();
+        objTrailParticleSystems = new ParticleSystem[followParticleSystems.Length];
+        trailParticleGameObjects = new GameObject[followParticleSystems.Length];
+        for (int i = 0; i < followParticleSystems.Length; i++)
+        {
+            // instantiated as a child to the gameobject's transform.
+            objTrailParticleSystems[i] = Instantiate(followParticleSystems[i], transform.position, transform.rotation, transform);
+            trailParticleGameObjects[i] = objTrailParticleSystems[i].gameObject;
+        }
     }
 
     public void Explode()
     {
-        PersistThenDestroyChild();
+        PersistThenDestroyChildren();
         CreateExplosionParticle();
     }
 
-    private void PersistThenDestroyChild()
+    private void PersistThenDestroyChildren()
     {
-        if (trailParticleGameObject == null) return;
+        for (int i = 0; i < followParticleSystems.Length; i++)
+        {
+            if (trailParticleGameObjects[i] == null) continue;
 
-        // unparents so when parent is destroyed, the child isn't.
-        trailParticleGameObject.transform.parent = null;
+            // unparents so when parent is destroyed, the child isn't.
+            trailParticleGameObjects[i].transform.parent = null;
 
-        // stops particlesystem looping. Assignment to main and emission is required due to interface limitations
-        ParticleSystem.MainModule main = objTrailParticleSystem.main;
-        main.loop = false;
-        ParticleSystem.EmissionModule emission = objTrailParticleSystem.emission;
-        emission.rateOverTime = 0f;
+            // stops particlesystem looping. Assignment to main and emission is required due to interface limitations
+            ParticleSystem.MainModule main = objTrailParticleSystems[i].main;
+            main.loop = false;
+            ParticleSystem.EmissionModule emission = objTrailParticleSystems[i].emission;
+            emission.rateOverTime = 0f;
 
-        // destroy the trail GameObject after the particleSystem's duration.
-        Destroy(trailParticleGameObject, followParticleSystem.main.duration);
+            // destroy the follow GameObject after the particleSystem's duration.
+            Destroy(trailParticleGameObjects[i], followParticleSystems[i].main.duration);
+        }
     }
 
     private void CreateExplosionParticle()
